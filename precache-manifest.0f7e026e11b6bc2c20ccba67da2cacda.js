ac8253eab9c754974c1186c1a9e96d7b31a95449
Abhishek Ray <apostate22@gmail.com>
2020-05-08 18:01:59 +0530

    Updates

---
+++ precache-manifest.0f7e026e11b6bc2c20ccba67da2cacda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d52e6307d42a40ad770e0cf232e075",
+    "url": "/rayabhishek/index.html"
+  },
+  {
+    "revision": "d83b49c1595a5d9dbc3b",
+    "url": "/rayabhishek/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "f33e6ad74576c1aeac0a",
+    "url": "/rayabhishek/static/css/main.8120b77b.chunk.css"
+  },
+  {
+    "revision": "d83b49c1595a5d9dbc3b",
+    "url": "/rayabhishek/static/js/2.9c328d4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rayabhishek/static/js/2.9c328d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33e6ad74576c1aeac0a",
+    "url": "/rayabhishek/static/js/main.ee1d0bd8.chunk.js"
+  },
+  {
+    "revision": "1dbecad7866fc4eeeff9",
+    "url": "/rayabhishek/static/js/runtime-main.536eaaa1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.911b00284dd154c86b8cecd456f81ad9.js"
+  "/rayabhishek/precache-manifest.0f7e026e11b6bc2c20ccba67da2cacda.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rayabhishek/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
