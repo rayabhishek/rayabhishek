d7b43a31717f44c22072e3c0e92305024118fc31
Abhishek Ray <apostate22@gmail.com>
2020-05-10 13:19:35 +0530

    Updates

---
+++ precache-manifest.e0e4acdad14ff6b0a65d7738a51ecf12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a716f2403e8d1762464aec0987f51c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4842fe36b17359b24350",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "01bfaa730947af2c6def",
+    "url": "/static/css/main.482d53e5.chunk.css"
+  },
+  {
+    "revision": "4842fe36b17359b24350",
+    "url": "/static/js/2.26588db3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.26588db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01bfaa730947af2c6def",
+    "url": "/static/js/main.7e067c59.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
